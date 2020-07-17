1eb17d7d7aa08b97c1a42b1f50aee120c5e562fb
randmon <ccmarques98@gmail.com>
2020-07-17 20:39:09 +0200

    new wood tile

---
+++ src/main/java/dev/randmon/wanderer/tiles/WoodTile.java
@@ -0,0 +1,10 @@
+package dev.randmon.wanderer.tiles;
+
+import dev.randmon.wanderer.gfx.Assets;
+
+public class WoodTile extends Tile{
+
+    public WoodTile(int id) {
+        super(Assets.wood, id);
+    }
+}
